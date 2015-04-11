2074c562f8e0986df2340cb94546f30cc92611d3
Felix van Oost <felix.vanoost@live.com>
2015-04-10 22:47:27 -0700

    Defined I/O pin assignments and added pin declarations

--- Reflow_Oven_Controller/Reflow_Oven_Controller.ino
+++ Reflow_Oven_Controller/Reflow_Oven_Controller.ino
@@ -3,7 +3,33 @@
 
 void setup() 
 {
-
+  #define THERM        0            // Define thermocouple input pin (A0)
+  #define JUNCTION     1            // Define cold junction input pin (A1)
+  
+  #define SETBUTTON    2            // Define pushbutton input pins (D2-D4)
+  #define INCBUTTON    3
+  #define DECBUTTON    4
+  #define STATE1LED    5            // Define LED output pins (D5-D8)
+  #define STATE2LED    6
+  #define STATE3LED    7
+  #define STATE4LED    8
+  #define BUZZER       9            // Define buzzer output pin (D9)
+  #define OVEN         10           // Define oven SSR pin (D10)
+  
+  pinMode(THERM, INPUT);            // Declare pins as inputs
+  pinMode(JUNCTION, INPUT);
+  pinMode(SETBUTTON, INPUT);
+  pinMode(INCBUTTON, INPUT);
+  pinMode(DECBUTTON, INPUT);
+  
+  pinMode(STATE1LED, OUTPUT);       // Declare pins as outputs
+  pinMode(STATE2LED, OUTPUT);
+  pinMode(STATE3LED, OUTPUT);
+  pinMode(STATE4LED, OUTPUT);
+  pinMode(BUZZER, OUTPUT);
+  pinMode(OVEN, OUTPUT);
+  
+  Serial.begin(9600);               // Initialise serial port at 9600 baud
 }
 
 void loop() 
