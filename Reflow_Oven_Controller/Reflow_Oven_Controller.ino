49398c9a247de171b402148cffc158ad806b15f9
Felix van Oost <felix.vanoost@live.com>
2015-04-10 23:00:33 -0700

    Added getJunctionTemp function

--- Reflow_Oven_Controller/Reflow_Oven_Controller.ino
+++ Reflow_Oven_Controller/Reflow_Oven_Controller.ino
@@ -3,33 +3,36 @@
 
 void setup() 
 {
-  #define THERM        0            // Define thermocouple input pin (A0)
-  #define JUNCTION     1            // Define cold junction input pin (A1)
   
-  #define SETBUTTON    2            // Define pushbutton input pins (D2-D4)
   #define INCBUTTON    3
   #define DECBUTTON    4
-  #define STATE1LED    5            // Define LED output pins (D5-D8)
   #define STATE2LED    6
   #define STATE3LED    7
   #define STATE4LED    8
-  #define BUZZER       9            // Define buzzer output pin (D9)
-  #define OVEN         10           // Define oven SSR pin (D10)
   
-  pinMode(THERM, INPUT);            // Declare pins as inputs
   pinMode(JUNCTION, INPUT);
   pinMode(SETBUTTON, INPUT);
   pinMode(INCBUTTON, INPUT);
   pinMode(DECBUTTON, INPUT);
   
-  pinMode(STATE1LED, OUTPUT);       // Declare pins as outputs
   pinMode(STATE2LED, OUTPUT);
   pinMode(STATE3LED, OUTPUT);
   pinMode(STATE4LED, OUTPUT);
   pinMode(BUZZER, OUTPUT);
   pinMode(OVEN, OUTPUT);
   
-  Serial.begin(9600);               // Initialise serial port at 9600 baud
+}
+
+// Description:		Obtains an analog reading for the cold junction from the LM335 and converts it to a temperature in Celsius 
+// Parameters:		-
+// Returns:		The cold junction temperature in Celsius
+int getJunctionTemp()
+{
+  int junctionReading = 0;
+  int junctionTemp = 0;
+  
+  junctionReading = analogRead(JUNCTION);                        // Obtain 10-bit cold junction reading from the ADC
+  return junctionTemp;
 }
 
 void loop() 
