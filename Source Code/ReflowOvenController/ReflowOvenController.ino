9861e76e7a244b3cd3ca87508a404a0277f66dc9
Felix van Oost <felix.vanoost@live.com>
2015-04-12 09:50:14 -0700

    Added global variables for thermal profile parameters

--- Source Code/ReflowOvenController/ReflowOvenController.ino
+++ Source Code/ReflowOvenController/ReflowOvenController.ino
@@ -5,35 +5,39 @@
 // Reflow Oven Controller for the Arduino
 // v1.0, Built 10/04/2015
 
+#define thermPin        0                                           // Define thermocouple input pin (A0)
+#define junctionPin     1                                           // Define cold junction input pin (A1)
+  
+#define setButtonPin    2                                           // Define pushbutton input pins (D2-D4)
+#define incButtonPin    3
+#define decButtonPin    4
+#define LED1Pin         5                                           // Define LED output pins (D5-D8)
+#define LED2Pin         6
+#define LED3Pin         7
+#define LED4Pin         8
+#define buzzerPin       9                                           // Define buzzer output pin (D9)
+#define ovenPin         10                                          // Define oven SSR pin (D10)
+
 void setup() 
 {
-  #define THERM        0                                         // Define thermocouple input pin (A0)
-  #define JUNCTION     1                                         // Define cold junction input pin (A1)
+  pinMode(thermPin, INPUT);                                         // Declare digital pins as inputs / outputs
+  pinMode(junctionPin, INPUT);
+  pinMode(setButtonPin, INPUT);
+  pinMode(incButtonPin, INPUT);
+  pinMode(decButtonPin, INPUT);
+  pinMode(LED1Pin, OUTPUT);
+  pinMode(LED2Pin, OUTPUT);
+  pinMode(LED3Pin, OUTPUT);
+  pinMode(LED4Pin, OUTPUT);
+  pinMode(buzzerPin, OUTPUT);
+  pinMode(ovenPin, OUTPUT);
   
-  #define SETBUTTON    2                                         // Define pushbutton input pins (D2-D4)
-  #define INCBUTTON    3
-  #define DECBUTTON    4
-  #define STATE1LED    5                                         // Define LED output pins (D5-D8)
-  #define STATE2LED    6
-  #define STATE3LED    7
-  #define STATE4LED    8
-  #define BUZZER       9                                         // Define buzzer output pin (D9)
-  #define OVEN         10                                        // Define oven SSR pin (D10)
+  int soakTemp = 150;                                               // Declare thermal profile variables and initialise to default values
+  int soakTime = 60;
+  int reflowTemp = 220;
+  int reflowTime = 45;
   
-  pinMode(THERM, INPUT);                                         // Declare pins as inputs
-  pinMode(JUNCTION, INPUT);
-  pinMode(SETBUTTON, INPUT);
-  pinMode(INCBUTTON, INPUT);
-  pinMode(DECBUTTON, INPUT);
-  
-  pinMode(STATE1LED, OUTPUT);                                    // Declare pins as outputs
-  pinMode(STATE2LED, OUTPUT);
-  pinMode(STATE3LED, OUTPUT);
-  pinMode(STATE4LED, OUTPUT);
-  pinMode(BUZZER, OUTPUT);
-  pinMode(OVEN, OUTPUT);
-  
-  Serial.begin(9600);                                            // Initialise serial port at 9600 baud
+  Serial.begin(9600);                                               // Initialise serial port at 9600 baud
 }
 
 // Description:		Obtains an analog reading for the cold junction from the LM335 and converts it to a temperature in Celsius 
@@ -44,8 +48,8 @@ int getJunctionTemp()
   int junctionReading = 0;
   int junctionTemp = 0;
   
-  junctionReading = analogRead(JUNCTION);                        // Obtain 10-bit cold junction reading from the ADC
-  junctionTemp = ((junctionReading / 1023) * 5 * 100) - 273;     // Convert reading to temperature in Celsius
+  junctionReading = analogRead(junctionPin);                        // Obtain 10-bit cold junction reading from the ADC
+  junctionTemp = ((junctionReading / 1023) * 5 * 100) - 273;        // Convert reading to temperature in Celsius
   return junctionTemp;
 }
 
